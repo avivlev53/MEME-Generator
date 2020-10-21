06132504feb885a32ec9d18576ac3530eff3176d
aviv levy <avivlev53@gmail.com>
2020-10-21 19:28:23 +0300

    new

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,26 @@
 'use strict'
+
+
+function onInit(){
+    createPics()
+    renderPic()
+    console.log(gImgs)
+}
+function onShowEditor(num) {
+    var elModal = document.querySelector('.modal')
+    elModal.style.display = 'block'
+    // gCanvas = document.querySelector('#my-canvas');
+    // gCtx = gCanvas.getContext('2d');
+    
+
+    
+}
+
+function renderPic(){
+    var elPic=document.querySelector('.pic')
+    var strHtml=gImgs.map(pic => {
+        return `<img onclick="onShowEditor(${pic.id})" src="img/${pic.id}.jpg" >`
+    })
+
+    elPic.innerHTML=strHtml.join(' ')
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,30 @@
 'use strict'
+
+const gImgs=[]
+const gMeme={}
+const gNumOfPics=18
+// var gCanvas;
+// var gCtx;
+
+
+// function drawImg() {
+//     var img = new Image()
+//     img.src = './img/1.jpg';
+//     img.onload = () => {
+//         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+//     }
+// }
+
+function createPics(){
+    for (let i =1;i<=gNumOfPics;i++){
+        gImgs.push(_createPic(i))
+    }
+}
+
+function _createPic(id){
+    let img= {
+        id,
+        url :`img/${id}.jpg`
+    } 
+    return img
+}
